a638c527e2863fb6af6207d2a0693d48ece4e8f1
Matthew Zhang <115035711+mattzhang80@users.noreply.github.com>
2024-11-06 16:31:06 -0500

    created schema.sql

---
+++ schema.sql
@@ -0,0 +1,39 @@
+-- schema.sql
+
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS items;
+DROP TABLE IF EXISTS cart;
+DROP TABLE IF EXISTS orders;
+
+CREATE TABLE users (
+    user_id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL
+);
+
+CREATE TABLE items (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    price REAL NOT NULL,
+    category TEXT NOT NULL
+);
+
+CREATE TABLE cart (
+    user_id INTEGER,
+    item_id INTEGER,
+    quantity INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE,
+    FOREIGN KEY (item_id) REFERENCES items (id) ON DELETE CASCADE,
+    PRIMARY KEY (user_id, item_id)
+);
+
+CREATE TABLE orders (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    status TEXT CHECK(status IN ('placed', 'claimed', 'fulfilled', 'cancelled')),
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    user_id INTEGER,
+    total_items INTEGER,
+    cart TEXT,
+    location TEXT,
+    timeline TEXT DEFAULT 'U-Store',
+    FOREIGN KEY (user_id) REFERENCES users (user_id)
+);
