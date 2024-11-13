f4d4038a9ee8f9e9109af5cc3aeb4267964cfa72
Connor Frank <conjfrnk@gmail.com>
2024-11-12 22:48:44 -0500

    formatting

--- top.py
+++ top.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
-''' top.py Author: Bob Dondero'''
+""" top.py Author: Bob Dondero"""
 
-#-----------------------------------------------------------------------
+# -----------------------------------------------------------------------
 # top.py
 # Author: Bob Dondero
-#-----------------------------------------------------------------------
+# -----------------------------------------------------------------------
 
 import flask
 
-app = flask.Flask('app', template_folder='templates')
+app = flask.Flask("app", template_folder="templates")
