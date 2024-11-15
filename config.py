e9eb546d17ee23c9c976add1d2b92dd0f2e290d0
Matthew Zhang <115035711+mattzhang80@users.noreply.github.com>
2024-11-15 14:29:01 -0500

    env

--- config.py
+++ config.py
@@ -9,6 +9,15 @@ import secrets
 
 SECRET_KEY = secrets.token_hex(32)
 
+import os
+
+# Base URL for CAS
+BASE_URL = os.getenv("BASE_URL", "http://localhost:8000")
+CAS_SERVER = "https://cas.princeton.edu"
+CAS_LOGIN_ROUTE = f"{CAS_SERVER}/login"
+CAS_LOGOUT_ROUTE = f"{CAS_SERVER}/logout"
+CAS_VALIDATE_ROUTE = f"{CAS_SERVER}/validate"
+CAS_SERVICE = f"{BASE_URL}/auth/cas"
 
 def get_debug_mode():
     """Determine debug mode from environment variable."""
